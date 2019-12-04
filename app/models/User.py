1d347024c8b3159429ee772a77835b891107ac15
Manorlds-Eaglespark <manorldsapiens@gmail.com>
2019-12-04 13:40:14 +0300

    ädd user model

---
+++ app/models/User.py
@@ -0,0 +1,17 @@
+
+class User(Model.db):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(155))
+    email = db.Column(db.String(200))
+    password = db.Column(db.String(255))
+    thumbnail = db.Column(db.String(255))
+    role = db.Column(db.String(20))
+    time_added = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __init__(self):
+        self.name = name
+        self.password = password
+        self.email = email
+        self.role = role
+        self.time_added
---
+++ manage.py
@@ -0,0 +1,12 @@
+import os
+from flask_script import Manager # class for handling a set of commands
+from flask_migrate import Migrate, MigrateCommand
+from app import db, app
+from app.models.user import User
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
